e3ae8879c9b6de0e4737de89445005cb65cabc04
marawanFawzy <marawan20191700616@cis.asu.edu.eg>
2022-12-06 22:08:54 +0200

    fixed error

--- app/src/main/java/com/example/myshoppingapp/AddNewProduct.java
+++ app/src/main/java/com/example/myshoppingapp/AddNewProduct.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myshoppingapp.firebase.Categories;
+import com.example.myshoppingapp.firebase.Products;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -19,14 +21,20 @@ import java.util.ArrayList;
 public class AddNewProduct extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner spinner;
     private ArrayList<String> paths = new ArrayList<>();
-    EditText catName;
+    EditText ProductName, ProductQuantity, price;
+    private String SelectedCategory;
+    Button buttonAddProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_product);
-        catName = findViewById(R.id.catName);
+        buttonAddProduct = findViewById(R.id.buttonAddProduct);
+        ProductName = findViewById(R.id.ProductName);
+        ProductQuantity = findViewById(R.id.ProductQuantity);
+        price = findViewById(R.id.price);
         getAllCategories();
+        paths.add("");
         spinner = findViewById(R.id.spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(AddNewProduct.this,
                 android.R.layout.simple_spinner_item, paths);
@@ -34,6 +42,34 @@ public class AddNewProduct extends AppCompatActivity implements AdapterView.OnIt
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+        buttonAddProduct.setOnClickListener(v -> {
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            if (SelectedCategory.equals(""))
+                Toast.makeText(this, "please choose a category first", Toast.LENGTH_SHORT).show();
+            else {
+                if (ProductName.getText().toString().equals(""))
+                    Toast.makeText(this, "please choose a name first", Toast.LENGTH_SHORT).show();
+                else if (ProductQuantity.getText().toString().equals(""))
+                    Toast.makeText(this, "please choose a Quantity first", Toast.LENGTH_SHORT).show();
+                else if (price.getText().toString().equals(""))
+                    Toast.makeText(this, "please choose a price first", Toast.LENGTH_SHORT).show();
+                else {
+                    db.collection("Categories").whereEqualTo("name", SelectedCategory).get().addOnSuccessListener(queryDocumentSnapshots -> {
+                        DocumentSnapshot d = queryDocumentSnapshots.getDocuments().get(0);
+
+                        if (d.exists()) {
+                            Categories temp = d.toObject(Categories.class);
+                            if (temp != null)
+                                SelectedCategory = temp.getId();
+                        }
+                        String id = db.collection("Products").document().getId().substring(0, 5);
+                        Products temp = new Products(id, Integer.parseInt(ProductQuantity.getText().toString()), SelectedCategory, Integer.parseInt(price.getText().toString()), ProductName.getText().toString());
+                        db.collection("Products").document(id).set(temp).addOnSuccessListener(unused -> Toast.makeText(AddNewProduct.this, "added product", Toast.LENGTH_SHORT).show());
+                    });
+
+                }
+            }
+        });
     }
 
     void getAllCategories() {
@@ -42,11 +78,11 @@ public class AddNewProduct extends AppCompatActivity implements AdapterView.OnIt
                 .addOnSuccessListener(queryDocumentSnapshots -> {
                     if (queryDocumentSnapshots.size() == 0) {
                         Toast.makeText(AddNewProduct.this, "add a category First ", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
+                    } else {
                         for (DocumentSnapshot d : queryDocumentSnapshots) {
                             Categories temp = d.toObject(Categories.class);
-                            paths.add(temp.getName());
+                            if (temp != null)
+                                paths.add(temp.getName());
                         }
                     }
                 });
@@ -54,12 +90,7 @@ public class AddNewProduct extends AppCompatActivity implements AdapterView.OnIt
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
-
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-
+        SelectedCategory = parent.getItemAtPosition(position).toString();
     }
 
     public void onNothingSelected(AdapterView<?> arg0) {
