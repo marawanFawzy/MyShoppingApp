86afdc5842349233f5bd631c8158e8285c5a7934
marawanFawzy <marawan20191700616@cis.asu.edu.eg>
2022-12-23 14:48:05 +0200

    uml

--- app/src/main/java/com/example/myshoppingapp/adminPage.java
+++ app/src/main/java/com/example/myshoppingapp/adminPage.java
@@ -3,7 +3,6 @@ package com.example.myshoppingapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
